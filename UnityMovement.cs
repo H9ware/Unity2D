e10492957283e565e55266ee10c4c77659d066e3
H9ware <120129541+H9ware@users.noreply.github.com>
2022-12-08 22:42:30 +0400

    Add files via upload

---
+++ UnityMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    bool OnGround = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2( < choose speed >, rb.velocity.y);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(- < choose speed >, rb.velocity.y);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.W) && OnGround == "true")
+        {
+            rb.velocity = new Vector2(rb.velocity.x, < choose jump power >);
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            OnGround = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            OnGround = false;
+        }
+    }
+}
